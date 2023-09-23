78d1688f0c917db3947245b534f61828094d8079
anpoliakov <Andrewlybimenko@gmail.com>
2023-09-23 13:13:45 +0300

    fix errors in the fields of models

---
+++ src/main/java/by/anpoliakov/models/Book.java
@@ -0,0 +1,11 @@
+package by.anpoliakov.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Book {
+    private int book_id;
+    private String name;
+    private String author;
+    private int year;
+}
---
+++ src/main/java/by/anpoliakov/models/Person.java
@@ -0,0 +1,12 @@
+package by.anpoliakov.models;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class Person {
+    private int person_id;
+    private String full_name;
+    private int birth_year;
+}
